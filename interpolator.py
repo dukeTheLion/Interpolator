ce817a278f107e4625dec2952e45c54d54b3ac90
Savio G. Mendonça (DUKE) <savio_gm@icloud.com>
2021-10-19 10:48:42 -0300

    Part2

--- interpolator.py
+++ interpolator.py
@@ -1,5 +1,10 @@
+from math import factorial
 from copy import copy
 from sympy import symbols, simplify
+
+
+def mean(lst: []):
+    return sum(lst) / len(lst)
 
 
 def lagrange_interpolator(x: [], y: [], f: float):
@@ -31,10 +36,11 @@ def lagrange_interpolator(x: [], y: [], f: float):
         yw.append(y[i] / temp)
         print(f'{temp:11.3f}  {y[i] / temp:11.3f}')
 
-    print(f'wd = {wd:.3f}\nsum(yi/wi) = {sum(yw):.3f}\np({f}) = {wd * sum(yw):.5f}')
+    print(f'wd = {wd:.3f}\nsum(yi/wi) = {sum(yw):.3f}\np({f}) = {wd * sum(yw):.5f}\n')
+    print('-' * 110, '->\n')
 
 
-def newton_interpolator(x: [], y: [], interpolator: float, degree: int):
+def newton_interpolator(x: [], y: [], interpolator: float):
     box = [copy(y)]
     boxt = []
     r = 0
@@ -49,10 +55,60 @@ def newton_interpolator(x: [], y: [], interpolator: float, degree: int):
 
     print('       y     ', end='')
 
-    for v in range(degree):
+    for v in range(len(x) - 1):
         print(f'    delta {v}  ', end='')
 
-    print('\n', '-' * 12, '-' * 12 * degree)
+    print('\n', '-' * 12, '-' * 12 * (len(x) - 1))
+
+    for k in range(len(box) - 1):
+        temp = []
+        for l in range(len(box) - k - 1):
+            print(f'{box[l][k]:8.2f}  ', end='')
+            temp.append(box[l][k])
+        boxt.append(temp)
+        print()
+
+    print(f'\nf({interpolator}) = ')
+
+    for w in range(len(x)):
+        if w == 0:
+            r = boxt[0][w]
+            print(' + ' if boxt[0][w] > 0 else ' - ', f'{abs(boxt[0][w])}')
+        else:
+            temp = 1
+            print(' + ' if boxt[0][w] > 0 else ' - ', f'{abs(boxt[0][w])}', end='')
+            for z in range(0, w):
+                temp *= (interpolator - x[z])
+                print(f'({interpolator - x[z]})', end='')
+            print()
+
+            r += (boxt[0][w] * temp)
+
+    print()
+    print(f'f({interpolator}) = {simplify(r)}\n')
+    print('-' * 110, '->\n')
+
+
+def gregory_newton_interpolator(x: [], y: [], interpolator: float):
+    box = [copy(y)]
+    boxt = []
+    r = 0
+    h = x[1] - x[0]
+    z = (interpolator - x[0]) / h
+
+    for i in range(len(x)):
+        temp = []
+        for j in range(0, len(x) - i - 1):
+            val = (box[i][j + 1] - box[i][j])
+            temp.append(val)
+        box.append(temp)
+
+    print('       y     ', end='')
+
+    for v in range(len(x) - 1):
+        print(f'    delta {v}  ', end='')
+
+    print('\n', '-' * 12, '-' * 12 * (len(x) - 1))
 
     for k in range(len(box) - 1):
         temp = []
@@ -62,46 +118,45 @@ def newton_interpolator(x: [], y: [], interpolator: float, degree: int):
         boxt.append(temp)
         print()
 
-    print(f'f({interpolator}) = ', end='')
+    print(f'\nf({interpolator}) = ')
 
-    for w in range(degree + 1):
+    for w in range(len(x)):
         if w == 0:
             r = boxt[0][w]
-            print(f'{boxt[0][w]}', end='')
+            print(' + ' if boxt[0][w] > 0 else ' - ', f'{abs(boxt[0][w])}')
         else:
             temp = 1
-            print(' + ' if boxt[0][w] > 0 else ' - ', f'{abs(boxt[0][w])}', end='')
-            for z in range(0, w):
-                temp *= (interpolator - x[z])
-                print(f'({interpolator - x[z]})', end='')
+            print(' + ' if boxt[0][w] > 0 else ' - ', f'({abs(boxt[0][w])} * ', end='')
+            for q in range(0, w):
+                temp *= (z - q)
+                print(f'({(z - q)})', end='')
+            print(f')/{factorial(w)}')
 
-            r += (boxt[0][w] * temp)
+            r += ((boxt[0][w] / factorial(w)) * temp)
 
     print()
     print(f'f({interpolator}) = {simplify(r)}')
+    print('-' * 110, '->\n')
 
 
-lagrange_interpolator(x=[0, 1, 3],
-                      y=[5, 5, 11],
-                      f=2)
+lagrange_interpolator(x=[4,7,10],
+                      y=[35,28,25],
+                      f=5)
 
-print()
+
+"""newton_interpolator(x=[-0.256, -0.039, 0.233, 0.562],
+                    y=[1.5, 1.6, 1.7, 1.8],
+                    interpolator=symbols('x'))
+
 
 newton_interpolator(x=[-0.256, -0.039, 0.233, 0.562],
                     y=[1.5, 1.6, 1.7, 1.8],
-                    interpolator=symbols('x'),
-                    degree=3)
+                    interpolator=0)"""
 
-print()
+"""gregory_newton_interpolator(x=[0, 15, 30, 45, 60],
+                            y=[112.5-50, 154.5-86, 195-146, 171-73.5, 95.5-50],
+                            interpolator=35)"""
 
-newton_interpolator(x=[-0.256, -0.039, 0.233, 0.562],
-                    y=[1.5, 1.6, 1.7, 1.8],
-                    interpolator=0,
-                    degree=3)
-
-print()
-
-newton_interpolator(x=[-1, 0, 1, 2],
-                    y=[2, 1, 0, 4],
-                    interpolator=-2,
-                    degree=3)
+"""newton_interpolator(x=[-1, 0, 0.5, 0.8, 1.2, 1.3, 1.5, 1.8, 2, 3],
+                    y=[7, 5, 6.25, 7.72, 10.52, 11.37, 13.25, 16.52, 19, 35],
+                    interpolator=symbols('x'))"""
